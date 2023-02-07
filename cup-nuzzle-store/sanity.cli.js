0d81f184a5973577a4f0e074cf0cd71c4a63f5f6
Zyaaco <Zyaaco1@gmail.com>
2023-02-07 21:57:12 +0200

    first commit

---
+++ cup-nuzzle-store/sanity.cli.js
@@ -0,0 +1,8 @@
+import {defineCliConfig} from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: '8rmstw6k',
+    dataset: 'production'
+  }
+})
---
+++ pages/success.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect } from "react";
+import Link from "next/link";
+import { BsBagCheckFill } from "react-icons/bs";
+
+import { useStateContext } from "../context/StateContext";
+import { client } from "../lib/client";
+
+import { runConfetti } from "../lib/utils";
+
+const Success = ({ bannerData }) => {
+  const { setCartItems, setTotalPrice, setTotalQuantities } = useStateContext();
+
+  useEffect(() => {
+    localStorage.clear();
+    setCartItems([]);
+    setTotalPrice(0);
+    setTotalQuantities(0);
+    runConfetti();
+  }, []);
+  return (
+    <div className="success-wrapper">
+      <div className="success">
+        <p className="icon">
+          <BsBagCheckFill />
+        </p>
+        <h2>Thank you for your order!</h2>
+        <p className="email-msg">Check your email inbox for the receipt.</p>
+        <p className="description">
+          If you have any questions please email{" "}
+          <a
+            className="email"
+            href={`mailto:${bannerData[0].email}`}
+            target="_blank"
+          >
+            {bannerData[0].email}
+          </a>
+        </p>
+        <Link href="/">
+          <button type="button" width="300px" className="btn">
+            Continue Shopping
+          </button>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export const getServerSideProps = async () => {
+  const bannerQuery = '*[_type == "banner"]';
+  const bannerData = await client.fetch(bannerQuery);
+
+  return {
+    props: { bannerData },
+  };
+};
+
+export default Success;
